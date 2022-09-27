6e0302106e6fa66a6ee447487197483fb9bf5b08
nellizi <masterliz9095@gmail.com>
2022-09-27 13:38:38 +0900

    샘플 회원, 샘플 상품 생성

---
+++ src/main/java/com/ll/exam/app_2022_09_22/app/base/DevInitData.java
@@ -0,0 +1,28 @@
+package com.ll.exam.app_2022_09_22.app.base;
+
+
+import com.ll.exam.app_2022_09_22.app.member.entity.Member;
+import com.ll.exam.app_2022_09_22.app.member.service.MemberSercive.MemberService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevInitData {
+    @Bean
+    public CommandLineRunner initData(MemberService memberService, ProductService productService) {
+        return args ->
+        {
+            String password = "{noop}1234";
+            Member member1 = memberService.join("user1", password, "user1@test.com");
+            Member member2 = memberService.join("user2", password, "user2@test.com");
+            Member member3 = memberService.join("user3", password, "user3@test.com");
+            Member member4 = memberService.join("user4", password, "user4@test.com");
+
+            productService.create("단가라 OPS", 68000, "청평화 A-1-15");
+            productService.create("쉬폰 OPS", 72000, "청평화 A-1-15");
+        };
+    }
+}
